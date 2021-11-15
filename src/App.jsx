e74786d670551a836fe9e150c7f29fba8fd66855
Bucky <bucky0112@gmail.com>
2021-11-15 15:44:05 +0800

    install leaflet

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import MainMap from './components/MainMap'
+
+function App () {
+  return (
+    <div className='App'>
+      <MainMap />
+    </div>
+  )
+}
+
+export default App
